8ab0098539d1cf17c5d66669bffc14981c2ffdbd
Monica Dahir <monicadahir@Monicas-MacBook-Pro.local>
2019-06-10 16:49:59 -0400

    initial index, css, js

---
+++ script.js
@@ -0,0 +1,68 @@
+function msgDisplay() {
+	  var msg = document.getElementById("horoscope");
+  if (msg.style.display === "none") {
+    msg.style.display = "block";
+  } else {
+    msg.style.display = "none";
+  }
+};
+
+function aquarius() {
+	msgDisplay();
+  document.getElementById("horoscope").innerHTML=("If you come across a cat today, take a moment to pet them. If you are allergic to cats, carry Benadryl.")
+}
+
+function pisces() {
+	msgDisplay();
+  document.getElementById("horoscope").innerHTML=("Be honest with yourself: cauliflower-pizza is not a real replacement for normal pizza.")
+}
+
+function aries() {
+	msgDisplay();
+  document.getElementById("horoscope").innerHTML=("Buy a nintendo switch.")
+}
+
+function taurus() {
+	msgDisplay();
+  document.getElementById("horoscope").innerHTML=("Double-check that your zipper is up after going to the bathroom today.")
+}
+
+function gemini() {
+	msgDisplay();
+  document.getElementById("horoscope").innerHTML=("It might be fun being a quadruple agent and overthrowing governments, but take a minute to relax today.")
+}
+
+function cancer() {
+	msgDisplay();
+  document.getElementById("horoscope").innerHTML=("Do NOT approach the stray cat standing in the middle of the road. Walk backwards away from it, do not turn around.")
+}
+
+function leo() {
+	msgDisplay();
+  document.getElementById("horoscope").innerHTML=("You might stub your toe today, don't wear open-toed shoes.")
+}
+
+function virgo() {
+	msgDisplay();
+  document.getElementById("horoscope").innerHTML=("You are never too old to color with Crayola Crayons.")
+}
+
+function libra() {
+	msgDisplay();
+  document.getElementById("horoscope").innerHTML=("Take an afternoon nap.")
+}
+
+function scorpio() {
+	msgDisplay();
+  document.getElementById("horoscope").innerHTML=("Chances of spilling coffee on your shirt are high today.")
+}
+
+function saggitarius() {
+	msgDisplay();
+  document.getElementById("horoscope").innerHTML=("Don't get baited into arguing with Flat Earthers in the Youtube comment section today.")
+}
+
+function capricorn() {
+	msgDisplay();
+  document.getElementById("horoscope").innerHTML=("Sing some Whitney Houston songs at 6:00am to annoy your neighbors that insist on vacuuming at 12:30am.")
+}
